6a484f4a17795933f099726155ea65db46875c43
Zeeshan goburdhun <goburdhunz@hotmail.co.uk>
2019-06-17 12:26:43 +0200

    added initial game logic woo-hoo!

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,29 @@
-console.log("Up and running!");
 
-var cardOne = "king"
-var cardTwo = "king"
+cards = ["queen", "queen", "king", "king"]
 
-var cardThree = "queen"
-var cardFour = "queen"
+cardsInPlay = []
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+cardOne = cards[0]
+
+cardsInPlay.push(cardOne);
+
+console.log('User flipped' + ' ' + cardsInPlay);
+
+cardTwo = cards[2]
+
+cardsInPlay.push(cardTwo);
+
+console.log('User flipped' + ' ' + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+}
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+}
+	else {
+		alert("Sorry, try again");
+}
+
+
+
 
