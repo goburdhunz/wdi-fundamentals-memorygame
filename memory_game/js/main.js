d57d856d20345e655a59595663ea1cc344a09e0e
Zeeshan goburdhun <goburdhunz@hotmail.co.uk>
2019-06-17 18:20:29 +0200

    added functional logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,26 +3,58 @@ cards = ["queen", "queen", "king", "king"]
 
 cardsInPlay = []
 
-cardOne = cards[0]
-
-cardsInPlay.push(cardOne);
-
-console.log('User flipped' + ' ' + cardsInPlay);
-
-cardTwo = cards[2]
-
-cardsInPlay.push(cardTwo);
-
-console.log('User flipped' + ' ' + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
-}
+function checkMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+  		alert("You found a match!");
+  }
+  else {
+  		alert("Sorry, try again");
+  }
+};
+
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+  if (cardsInPlay.length === 2) {
+  }
+  checkMatch();
+};
+
+
+flipCard(0);
+flipCard(2);
+
+function advice(checkMatch) {
+	if (checkMatch === false) {
+		console.log("Sorry, try again.");
+	}
+
 }
-	else {
-		alert("Sorry, try again");
-}
+
+advice(false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
