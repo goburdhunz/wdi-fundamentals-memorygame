bc05b4fc65014e0238ee03aa70e07b9f45ec0d12
Zeeshan goburdhun <goburdhunz@hotmail.co.uk>
2019-06-18 21:46:55 +0200

    added object logic to game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,25 @@
-
-cards = ["queen", "queen", "king", "king"]
+var cards = [
+{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+},
+{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+},
+{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+},
+{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+}
+];
 
 cardsInPlay = []
 
@@ -14,8 +34,10 @@ function checkMatch() {
 
 
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 
   if (cardsInPlay.length === 2) {
   }
@@ -26,14 +48,19 @@ function flipCard(cardId) {
 flipCard(0);
 flipCard(2);
 
-function advice(checkMatch) {
-	if (checkMatch === false) {
-		console.log("Sorry, try again.");
-	}
 
-}
 
-advice(false);
+
+// function advice(checkMatch) {
+// 	if (checkMatch === false) {
+// 		console.log("Sorry, try again.");
+// 	}
+
+// }
+
+// advice(false);
+
+
 
 
 
